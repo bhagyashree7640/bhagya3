b70a4bffd0748338aa00553db4801e574da3453a
bhagyashree7640 <155372364+bhagyashree7640@users.noreply.github.com>
2024-08-27 13:45:56 +0000

    feat: initiated the project

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 contract TestNFT is ERC721, ERC721Burnable {
     constructor()
-        ERC721("bhagya3","bhag3")
+        ERC721("bhagya3","bha3")
     {}
 
     function safeMint(address to, uint256 tokenId) public {
